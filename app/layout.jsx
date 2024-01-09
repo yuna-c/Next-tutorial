a65e067189476e2ebac678ac8985728fec92a110
yuna <yuna744@gmail.com>
2024-01-09 13:26:59 +0900

    0109 메뉴 설정

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Header from "./components/Header/header";
 import "./styles/globals.scss";
 
 export const metadata = {
@@ -9,9 +10,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <header>Menu</header>
+        <Header />
         {children}
       </body>
     </html>
   );
 }
+
+/*
+  Next에서의 기능별 컴포넌트 구분
+  Page Component 
+  --페이지로 출력될 틀 컴포넌트 
+  --about/page.jsx, gallery/page.jsx
+  --따로 설정을 하지 않으면 기본적으로 Server에서 동작되서 prepage로 사전 렌더링
+  Partial Component 
+  --페이지 안쪽에서 호출되면서 부분적인 UI나 기능을 담당하는 컴포넌트 
+  --componets>header.jsx, componets/popup.jsx 같은 것들
+*/
