b77675612f5b4f1fc95829d6d878c690b5c9b183
yuna <yuna744@gmail.com>
2024-01-09 16:28:31 +0900

    0109 전역 스타일 연결

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
       <body>
         <Header />
         {children}
+        <button className="btn">BTN BTN</button>
       </body>
     </html>
   );
