8cc5fe1f4f9cc74a2bedec2658ae0ccc36b548d0
yuna <yuna744@gmail.com>
2024-01-11 16:08:36 +0900

    0111 클라이언트로부터 서버쪽에 글저장 요청 보내기

--- app/post/write/page.jsx
+++ app/post/write/page.jsx
@@ -11,12 +11,27 @@ export default function Write() {
     const { name, value } = e.target; //{'title','제목값'}
     setPost({ ...Post, [name]: value });
   };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("/api/requestPost", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(Post),
+    });
+
+    if (response.ok) {
+      alert("글 저장에 성공했습니다.");
+    } else {
+      alert("글 저장에 실패했습니다.");
+    }
+  };
 
   return (
     <section className={clsx(styles.write)}>
       <h1>Write Post</h1>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="title"
@@ -28,9 +43,12 @@ export default function Write() {
           name="content"
           cols="30"
           rows="3"
+          placeholder="content"
           value={Post.content}
           onChange={handleChange}
         ></textarea>
+
+        <button>Send Post</button>
       </form>
     </section>
   );
