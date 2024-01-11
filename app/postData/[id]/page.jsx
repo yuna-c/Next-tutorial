e73a2b75567a79fbfa805659127ee41a3d6a169a
yuna <yuna744@gmail.com>
2024-01-11 12:35:35 +0900

    0111 복수개의 파일받기 위한 [...그룹화]

--- app/postData/[id]/page.jsx
+++ app/postData/[id]/page.jsx
@@ -1,9 +1,8 @@
-"use client";
-
 export default function PostDetail({ params }) {
-  //다이나믹 라우터 폴더명을 [...변수명]으로 설정할 경우
-  //기본 라우터명 뒤의 모든 값을 배열로 그룹화해서 params로 전달가능 ('use client'설정 필요)
-  //post/abc/1 --> {id:['abc','1']}
-  console.log(params);
-  return <div>{/* <h2>{id}번째 게시글 상세페이지</h2> */}</div>;
+  const { id } = params;
+  return (
+    <div>
+      <h2>{id}번째 게시글 상세페이지</h2>
+    </div>
+  );
 }
